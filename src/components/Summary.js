06319631b6511143118d7c982570f4fefbd376ec
Dave J <29786887+daveswork@users.noreply.github.com>
2024-08-21 14:29:39 -0400

    Formatted aggregate income and expenses figures.

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,6 +4,11 @@ import { useOutletContext } from "react-router-dom"
 function Summary(){
     const {expenditureList, incomeSourceList} = useOutletContext()
 
+    const USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    })
+
     const initialValue = 0
 
     const totalExpenses = expenditureList.reduce((total, expense) => total + Number(expense.amount), initialValue)
@@ -18,8 +23,8 @@ function Summary(){
     return(
         <div>
             <h1>Summary</h1>
-            <h2>Total Income: {totalIncome}</h2>
-            <h2>Total expenses: {totalExpenses}</h2>
+            <h2>Total Income: {USDollar.format(totalIncome)}</h2>
+            <h2>Total expenses: {USDollar.format(totalExpenses)}</h2>
         </div>
     )
 }
